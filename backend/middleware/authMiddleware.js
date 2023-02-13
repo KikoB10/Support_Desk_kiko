dcda1405c52558cba3b5b2f76369a26f2cb6f1cd
KikoB10 <burten.kiko@gmail.com>
2023-02-13 06:48:40 -0600

    initial commit backend setup

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      //Get token from header (must remember its in the Bearer Token, so we make it into an array nd then we split with the space (' ') so that Bearer is the first item ([0]) in the array and then the token is second [1])
+      token = req.headers.authorization.split(' ')[1];
+      //Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      //get user from token
+      req.user = await User.findById(decoded.id).select('-password');
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error('Not authorized');
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error('Not authorized');
+  }
+});
+
+module.exports = { protect };
---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+//Connect to database
+connectDB();
+
+const app = express();
+
+//get data from the body?
+app.use(express.json()); //allows us to send raw json
+app.use(express.urlencoded({ extended: false })); //accepts urlencoded form
+
+app.get('/', (req, res) => {
+  res.status(201).json({ message: 'Welcome to the SUpport Desk API' });
+});
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
