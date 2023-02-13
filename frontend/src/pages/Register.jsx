b80cf17f85a74ee166f2c90c42d556cda9ab939f
KikoB10 <burten.kiko@gmail.com>
2023-02-13 08:03:42 -0600

    Frontend setup, Pages: home, login, register and Header component

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { useState } from 'react';
+import { FaUser } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+
+function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    passwor2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (password !== password2) {
+      toast.error('Passwords do not match');
+    }
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser />
+          Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              value={name}
+              id="name"
+              name="name"
+              onChange={onChange}
+              placeholder="Enter your name"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              value={email}
+              id="email"
+              name="email"
+              onChange={onChange}
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              value={password}
+              id="password"
+              name="password"
+              onChange={onChange}
+              placeholder="Enter password"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              value={password2}
+              id="password2"
+              name="password2"
+              onChange={onChange}
+              placeholder="Confirm password"
+              required
+            />
+            <div className="form-group">
+              <button className="btn btn-block">Submit</button>
+            </div>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+}
+
+export default Register;
