abbf443bccede6cda647cbc4967544cdde59d9a0
KikoB10 <burten.kiko@gmail.com>
2023-02-16 16:42:46 -0600

    NewTicket creation page PrivateRoute for creating New Ticket so only loggedIn users can create useAuthStatus hook created for this as well

---
+++ frontend/src/hooks/useAuthStatus.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+export const useAuthStatus = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setCheckingStatus] = useState(true);
+
+  const { user } = useSelector((state) => state.auth); //getting the user through redux but ultimately form the node express backend
+
+  useEffect(() => {
+    if (user) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+    setCheckingStatus(false);
+  }, [user]);
+
+  return { loggedIn, checkingStatus };
+};
